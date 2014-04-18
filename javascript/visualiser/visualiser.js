481174ee4e41f94fac403a565e44cbf5d4c2415e
Yousef Amar <yousefamar@gmail.com>
2014-04-18 10:54:15 +0100

    Display last occurrence of every message type in browser

--- javascript/visualiser/visualiser.js
+++ javascript/visualiser/visualiser.js
@@ -4,10 +4,21 @@ VIS.main = (function () {
   var shoe = require('shoe');
   var through = require('through');
 
+  var divs = {};
+
   return function () {
     var stream = shoe('/racedata');
     stream.pipe(through(function (data) {
-        console.log(JSON.parse(data));
+      var msg = JSON.parse(data);
+
+      if (msg.msgType in divs) {
+        divs[msg.msgType].innerHTML = data;
+      } else {
+        document.body.appendChild(document.createTextNode(msg.msgType+': '));
+        var div = divs[msg.msgType] = document.createElement('div');
+        div.innerHTML = data;
+        document.body.appendChild(div);
+      }
     }));
   };
 })();
